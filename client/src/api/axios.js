cb57d221cdfa5a9db4ca7cd466dec2d39f9ec9ed
Zayn Ali <jahma72441@gmail.com>
2026-03-15 21:14:34 +0300

    update API URL for production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../store/authStore';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://whatsapp-crm-iflz.onrender.com//api',
   headers: { 'Content-Type': 'application/json' },
 });
 
